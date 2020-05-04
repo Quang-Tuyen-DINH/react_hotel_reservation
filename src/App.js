2fe4b36c72e709f10433d4ccbe2985f813e9fac1
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-05-05 00:49:16 +0200

    Initialize Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <div>
-      hello from app
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/rooms/" component={Rooms} />
+        <Route path="/single-room" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Error = () => {
+  return (
+    <div>
+      Hello from Error page
+    </div>
+  );
+}
+
+export default Error;
